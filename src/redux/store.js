a8e64514737663e25c994087eec49b5ee184953b
rok0705 <leeseon0503@gmail.com>
2023-10-02 21:36:51 -0400

    save latest code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducers from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-let store = createStore(rootReducers, applyMiddleware(thunk));
+let store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
